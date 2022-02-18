f82573a91317a85a54f8421dd3c546ece85638b2
Larbi Amine Mohamed <55483194+LarbiAmineGIT@users.noreply.github.com>
2022-02-18 19:20:39 +0100

    Add files via upload

---
+++ build/CMakeFiles/tinyraytracer.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/amine/Documents/cours/M1/rdv/tinyraytracer/model.cpp" "/home/amine/Documents/cours/M1/rdv/tinyraytracer/build/CMakeFiles/tinyraytracer.dir/model.cpp.o"
+  "/home/amine/Documents/cours/M1/rdv/tinyraytracer/tinyraytracer.cpp" "/home/amine/Documents/cours/M1/rdv/tinyraytracer/build/CMakeFiles/tinyraytracer.dir/tinyraytracer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../lib/stb"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
