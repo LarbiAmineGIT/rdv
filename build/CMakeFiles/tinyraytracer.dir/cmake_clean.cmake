bec7acca9839385f8977abec666a888bb8eadbf0
Fthiems <florian.thiems@gmail.com>
2022-02-20 18:33:02 +0100

    added submodule

---
+++ build/CMakeFiles/tinyraytracer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tinyraytracer.dir/model.cpp.o"
+  "CMakeFiles/tinyraytracer.dir/tinyraytracer.cpp.o"
+  "tinyraytracer"
+  "tinyraytracer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tinyraytracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
